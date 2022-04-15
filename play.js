1dade37a02f7151d131c9c304d01b359ee98db72
Thushara <thusharan@hotmail.com>
2022-04-14 20:58:24 -0400

    added message to console

--- play.js
+++ play.js
@@ -1,12 +1,18 @@
-//main file where you will launch the game client
 const net = require("net");
 
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    "host": "192.168.0.15",
+    "port": "50541"
+  });
+  conn.on("connect", () => {
+  console.log("you ded cuz you idled");
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
   return conn;
 };
-
 console.log("Connecting ...");
 connect();
